0fdb9146fe1112794ba0bf9acddcf3f0ca666a8c
Graff Alexandre <alexgraff67@gmail.com>
2024-06-16 16:58:57 +0200

    0.0.0 initialisation du projet

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      < Header />
+      <main className="flex flex-wrap justify-center items-center flex-grow bg-gray-100 px-10 md:flex-nowrap">
+        {/* < Sidebar /> */}
+        {/* <Visualization /> */}
+      </main>
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
